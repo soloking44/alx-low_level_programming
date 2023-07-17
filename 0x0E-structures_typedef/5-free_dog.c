33830c84fae1737c6fd4c6ea6176c6449d5c95da
soloking44 <solop8483@gmail.com>
2023-07-17 06:44:56 -0700

    5-free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs
+ *
+ * @d: a pointer to the dog array
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
