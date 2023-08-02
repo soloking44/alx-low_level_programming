b03ea03185f7d889a725e02f996239200298a60e
soloking44 <solop8483@gmail.com>
2023-08-02 08:16:55 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,14 +10,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 /**
  * struct listp_s - singly linked list
  * @p: pointers of nodes
@@ -30,7 +28,6 @@ typedef struct listp_s
 	void *p;
 	struct listp_s *next;
 } listp_t;
-
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 size_t listint_len(const listint_t *h);
