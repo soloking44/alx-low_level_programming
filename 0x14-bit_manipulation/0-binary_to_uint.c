b7ae76319d37ce308e77ffb56adc4f4c18367361
soloking44 <solop8483@gmail.com>
2023-08-03 05:30:19 -0700

    0-binary_to_uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that displays a binary number to an unsigned int.
+ * @b: double pointer to the string of 0's and 1's
+ * Return: an unsigned int number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int uint;
+	int len, binary;
+
+	if (!b)
+		return (0);
+
+	uint = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, binary = 1; len >= 0; len--, binary *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			uint += binary;
+		}
+	}
+	return (uint);
+}
