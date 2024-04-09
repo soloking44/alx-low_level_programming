9296ac1187903ae6a28c58527984fa36116f63fa
soloking44 <solop8483@gmail.com>
2024-04-09 01:15:53 -0700

    1-binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,61 +1,30 @@
 #include "search_algos.h"
 
 /**
- * recursive_search - Checks for value within an array 
- * integers used for Binary search algorithm
- *
- *
- * @array: Entry array
- * @size: The size of an array
- * @value: The value to search for
- * Return: Position of the number
- */
-int recursive_search(int *array, size_t size, int value)
+  * linear_search - Checks for a value in an array
+  *                 of integers used linear search.
+  * @array: A reference to the new element of an array to check.
+  * @size: The value of elements within an array.
+  * @value: The value to check for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         else, the first index where the value is located.
+  *
+  * Description: Shows the value eche time is compared within array.
+  */
+int linear_search(int *array, size_t size, int value)
 {
-	size_t half = size / 2;
 	size_t k;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
-
-	printf("Searching in array");
 
 	for (k = 0; k < size; k++)
-		printf("%s %d", (k == 0) ? ":" : ",", array[k]);
+	{
+		printf("Value checked array[%ld] = [%d]\n", k, array[k]);
+		if (array[i] == value)
+			return (k);
+	}
 
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
-}
-
-/**
- * binary_search - Function of binary_search that yield
- * to position of the number
- *
- * @array: Entry array
- * @size: A size of an array
- * @value: The value to search for
- * Return: Position of the number
- */
-int binary_search(int *array, size_t size, int value)
-{
-	int index;
-
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
-		return (-1);
-
-	return (index);
+	return (-1);
 }
